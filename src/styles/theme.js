688fc81690cabdd9bed818bbf1147f9c920f3ecc
Alisa Promthep <alisa.promthep@gmail.com>
2023-08-11 11:47:29 -0700

    installed and partially set up material ui themeing file, and set up routing

---
+++ src/styles/theme.js
@@ -0,0 +1,19 @@
+import {createTheme} from '@mui/material/styles';
+
+const theme = createTheme({
+    palette:{
+        // primary:{
+        //     main:'#2E2252',
+        // },
+        // secondary:{
+        //     main:'',
+        // },
+    },
+    typography:{
+        // h1:{
+
+        // }
+    }
+})
+
+export default theme;
