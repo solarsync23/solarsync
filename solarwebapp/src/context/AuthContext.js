aca42340f8ee196fd12898348503dcfeff6d3a83
Jaanhvi18 <93530844+Jaanhvi18@users.noreply.github.com>
2023-08-16 13:42:23 -0400

    authcontext.js  intial commit

--- solarwebapp/src/context/AuthContext.js
+++ solarwebapp/src/context/AuthContext.js
@@ -1,14 +1,54 @@
-import { createContext } from "react";
-import GetFireBaseAdmin from "../../lib/firebase";
-export const AuthContext = createContext(null);
+"use client";
+import React, { useContext, createContext,useState, useRef, useEffect } from 'react'
+import { auth } from '../utils/firebase'
+import { signInWithEmailAndPassword, createUserWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth'
 
-function Context({ children }) {
-    const {auth, db} = GetFireBaseAdmin();
-  return (
-    <AuthContext.Provider value={{ auth, db }}>
-      {children}
-    </AuthContext.Provider>
-  );
+
+const AuthContext = React.createContext();
+
+export function useAuth() {
+    return useContext(AuthContext);
 }
 
-export default Context;
+export function AuthProvider({children}) {
+    const [currentUser, setCurrentUser ] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const userInfo = useRef();
+
+    function signUp(email, password){
+        createUserWithEmailAndPassword(auth, email, password)
+        return
+    }
+
+    function logIn(email, password) {
+        signInWithEmailAndPassword(auth, email, password)
+        return
+    }
+
+    function logOut() {
+        signOut(auth)
+        return
+    }
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, async user => {
+            setCurrentUser(user)
+            setLoading(false)
+        })
+        return unsubscribe
+    })
+
+    const value = {
+        currentUser,
+        logIn,
+        signUp,
+        logOut,
+        userInfo
+    }
+
+    return (
+        <AuthContext.Provider value={value}>
+            {!loading && children}
+        </AuthContext.Provider>
+    )
+}
