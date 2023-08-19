8088b16530e4a4adf9b1873090dea4cecc36e291
Alisa Promthep <alisa.promthep@gmail.com>
2023-08-19 12:47:11 -0700

    homepage icon finished

--- solarwebapp/tailwind.config.ts
+++ solarwebapp/tailwind.config.ts
@@ -7,6 +7,10 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    colors:{
+      'electric-green':'#8EFFC2',
+      'darkblue':'#031141',
+    },
     extend: {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
