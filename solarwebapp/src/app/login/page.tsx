2480431c137cda0eb804cbd6aa58828598ea54c3
Jaanhvi18 <93530844+Jaanhvi18@users.noreply.github.com>
2023-08-21 14:36:46 -0400

    login page error for vercel

--- solarwebapp/src/app/login/page.tsx
+++ solarwebapp/src/app/login/page.tsx
@@ -11,67 +11,57 @@ const LoginPage = () => {
   const router = useRouter();
   const handleLogin = (e: { preventDefault: () => void; }) => {
     e.preventDefault();
-  const auth = getAuth();
-
+    const auth = getAuth();
     
     signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
         const user = userCredential.user;
         router.push('/preferences');
-        console.log(user);
-
     }).catch((error) => {
-        const errorMessage = "Invalid email/password";
-        console.log(errorMessage);
+        const errorMessage = error.message;
+        alert(errorMessage)
     });
     
 };
 
-
-
   return (
-    <div className='h-screen flex flex-col justify-around items-center bg-darkblue text-white'>
-      <Logo/>
-      <div className="w-full flex justify-center items-center bg-gradient-to-b from-indigo-950 to-indigo-900">
-        <div className="w-full max-w-md">
-          <h2 className="text-peach text-2xl text-center font-bold mb-4">Log In</h2>
-          <form onSubmit={handleLogin}
-          className='flex-center flex-col p-4'
-          >
-              <input
-              placeholder='Email Address'
-                type="email"
-                id="email"
-                className="input-field w-full focus:outline-none focus:ring focus:border-blue-300"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <input
-              placeholder='password'
-                type="password"
-                id="password"
-                className="w-full input-field focus:outline-none focus:ring focus:border-blue-300"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <button
-                type="button"
-                className="text-white w-full text-center m-2 hover:text-white"
-                onClick={() =>router.push("/reset")}
-              >
-                Forgot Password?
-              </button>
-            <div className="w-full flex-col justify-center items-center mt-4">
-              <button type="submit" className="button-pill">
-                Log In
-              </button>
-              <button className='button-pill'>Register</button>
-            </div>
-            <button className='w-full text-center m-2'>Continue as guest</button>
-          </form>
-    
-        </div>
+    <div className="flex justify-center items-center h-screen bg-gradient-to-b from-indigo-950 to-indigo-900">
+      <div className="w-full max-w-md">
+        <h2 className="text-peach text-2xl text-center font-bold mb-4">Log In</h2>
+        <form onSubmit={handleLogin}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block mb-1">Email Address</label>
+            <input
+              type="email"
+              id="email"
+              className="bg-peach text-black w-full px-4 py-2 rounded border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              required
+            />
+          </div>
+          <div className="mb-4">
+            <label htmlFor="password" className="text-peach block mb-1">Password</label>
+            <input
+              type="password"
+              id="password"
+              className="bg-peach text-black w-full px-4 py-2 rounded border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+            />
+          </div>
+          <button type="submit" className="w-full py-2 bg-indigo-900 text-white rounded hover:bg-indigo-800">Log In</button>
+          <div className="flex justify-center items-center mt-4">
+            <button
+              type="button"
+              className="text-white hover:text-white"
+              onClick={() =>router.push("/reset")}
+            >
+              Forgot Password?
+            </button>
+          </div>
+        </form>
+  
       </div>
     </div>
   );
