100212b9233a295426428f3a9ddbe86c586025c3
Alisa Promthep <alisa.promthep@gmail.com>
2023-08-22 22:14:49 -0700

    resolved conflict

--- solarwebapp/src/app/login/page.tsx
+++ solarwebapp/src/app/login/page.tsx
@@ -3,16 +3,9 @@ import React, { useState } from 'react';
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
 import '/src/utils/firebase.js';
 import Logo from '@/components/Logo/Logo';
+import Link from 'next/link';
 
 import { useRouter } from 'next/navigation';
-<<<<<<< HEAD
-
-=======
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Logo from '../../components/Logo/Logo';
-import Link from 'next/link';
->>>>>>> develop-ap
 
 const LoginPage = () => {
   const [email, setEmail] = useState('');
@@ -33,45 +26,7 @@ const LoginPage = () => {
 };
 
   return (
-<<<<<<< HEAD
-    <div className="flex justify-center items-center h-screen bg-darkblue">
-      <div className="w-full max-w-md">
-        <h2 className="text-peach text-2xl text-center font-bold mb-4">Log In</h2>
-        <form onSubmit={handleLogin}>
-          <div className="mb-4">
-            <label htmlFor="email" className="block mb-1">Email Address</label>
-            <input
-              type="email"
-              id="email"
-              className="bg-peach text-black w-full px-4 py-2 rounded border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="password" className="text-peach block mb-1">Password</label>
-            <input
-              type="password"
-              id="password"
-              className="bg-peach text-black w-full px-4 py-2 rounded border-gray-300 focus:outline-none focus:ring focus:border-blue-300"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="w-full py-2 bg-indigo-900 text-white rounded hover:bg-indigo-800">Log In</button>
-          <div className="flex justify-center items-center mt-4">
-            <button
-              type="button"
-              className="text-white hover:text-white"
-              onClick={() =>router.push("/reset")}
-            >
-              Forgot Password?
-            </button>
-          </div>
-        </form>
-=======
+
     <div className='h-screen flex flex-col justify-around items-center bg-darkblue'>
       <Logo/>
       <div className="w-full flex justify-center items-center bg-gradient-to-b from-indigo-950 to-indigo-900">
@@ -113,10 +68,9 @@ const LoginPage = () => {
             <Link href='/map' className='w-full text-center m-2 text-white'>Continue as guest</Link>
           </form>
         </div>
-        <ToastContainer />
->>>>>>> develop-ap
       </div>
     </div>
+    
   );
 };
 
--- solarwebapp/src/app/page.tsx
+++ solarwebapp/src/app/page.tsx
@@ -1,10 +1,11 @@
-import Transition from '@/components/Transition/Transition';
+import Logo from '@/components/Logo/Logo';
+import Link from 'next/link';
 
 export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24 bg-darkblue">
-      <Transition/>
+      <Link href='/login'><Logo/></Link>
     </main>
   )
 }
